1f495290981e635edbd6ebd5e77ca03fa21b446a
Marek Gieron <mjgieron@gmail.com>
2016-10-25 20:33:51 +0200

    proba2

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,3 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-#proba
